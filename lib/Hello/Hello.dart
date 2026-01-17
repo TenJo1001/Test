54e7b2d6b892b715456ece8a5b4a0f338d74e18b
TenJo1001 <khatlyheng6@gmail.com>
2026-01-17 11:53:25 +0700

    no message

--- lib/Hello/Hello.dart
+++ lib/Hello/Hello.dart
@@ -10,6 +10,8 @@ class Hello extends StatefulWidget {
 class _HelloState extends State<Hello> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      
+    );
   }
 }
